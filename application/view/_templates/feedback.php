f3ed36c6231b60c4a718411a4edcde771bee81a5
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-11-17 14:33:13 -0500

    Add debug feedback

--- application/view/_templates/feedback.php
+++ application/view/_templates/feedback.php
@@ -3,6 +3,7 @@
 // get the feedback (they are arrays, to make multiple positive/negative messages possible)
 $feedback_positive = Session::get('feedback_positive');
 $feedback_negative = Session::get('feedback_negative');
+$feedback_debug    = Session::get('feedback_debug');
 
 // echo out positive messages
 if (isset($feedback_positive)) {
@@ -15,5 +16,11 @@ if (isset($feedback_positive)) {
 if (isset($feedback_negative)) {
     foreach ($feedback_negative as $feedback) {
         echo '<div class="feedback error">'.$feedback.'</div>';
+    }
+}
+
+if (isset($feedback_debug) && Environment::get() === 'development') {
+    foreach($feedback_debug as $feedback) {
+        echo '<script>console.log("'.$feedback.'");</script>';
     }
 }
