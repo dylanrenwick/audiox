d87eb58f697e04111aeaacb5028e61419e3f6847
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-11-17 14:37:57 -0500

    Restructure header navbar and remove logo

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -13,38 +13,16 @@
     <!-- wrapper, to center website -->
     <div class="wrapper">
 
-        <!-- logo -->
-        <div class="logo"></div>
-
         <!-- navigation -->
         <ul class="navigation">
             <li <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?> >
                 <a href="<?php echo Config::get('URL'); ?>index/index">Index</a>
             </li>
-            <li <?php if (View::checkForActiveController($filename, "profile")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>profile/index">Profiles</a>
-            </li>
-            <?php if (Session::userIsLoggedIn()) { ?>
-                <li <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>dashboard/index">Dashboard</a>
-                </li>
-                <li <?php if (View::checkForActiveController($filename, "note")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>note/index">My Notes</a>
-                </li>
-            <?php } else { ?>
-                <!-- for not logged in users -->
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>login/index">Login</a>
-                </li>
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "register/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>register/index">Register</a>
-                </li>
-            <?php } ?>
         </ul>
 
         <!-- my account -->
         <ul class="navigation right">
-        <?php if (Session::userIsLoggedIn()) : ?>
+        <?php if (Session::userIsLoggedIn()) { ?>
             <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
                 <a href="<?php echo Config::get('URL'); ?>user/index">My Account</a>
                 <ul class="navigation-submenu">
@@ -68,12 +46,17 @@
                     </li>
                 </ul>
             </li>
-            <?php if (Session::get("user_account_type") == 7) : ?>
-                <li <?php if (View::checkForActiveController($filename, "admin")) {
-                    echo ' class="active" ';
-                } ?> >
+            <?php if (Session::get("user_account_type") == 7) { ?>
+                <li <?php if (View::checkForActiveController($filename, "admin")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>admin/">Admin</a>
                 </li>
-            <?php endif; ?>
-        <?php endif; ?>
+            <?php } ?>
+        <?php } else { ?>
+            <li>
+                <a href="<?php echo Config::get('URL'); ?>login/index">Log In</a>
+            </li>
+            <li>
+                <a href="<?php echo Config::get('URL'); ?>register/index">Sign Up</a>
+            </li>
+        <?php } ?>
         </ul>
