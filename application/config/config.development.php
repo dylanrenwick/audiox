d0f4d75dd2032c71bb6924fc8faaa8d27427f872
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-11-18 21:46:49 -0500

    Include API_ROOT and PATH_API in default config file

--- application/config/config.development.php
+++ application/config/config.development.php
@@ -34,12 +34,14 @@ return array(
      * URL manually. This should then look like 'http://192.168.33.44/' ! Note the slash in the end.
      */
     'URL' => 'http://' . $_SERVER['HTTP_HOST'] . str_replace('public', '', dirname($_SERVER['SCRIPT_NAME'])),
+	'API_ROOT' => 'api',
     /**
      * Configuration for: Folders
      * Usually there's no reason to change this.
      */
     'PATH_CONTROLLER' => realpath(dirname(__FILE__).'/../../') . '/application/controller/',
     'PATH_VIEW' => realpath(dirname(__FILE__).'/../../') . '/application/view/',
+	'PATH_API' => realpath(dirname(__FILE__).'/../../') . '/application/controller/api/',
     /**
      * Configuration for: Avatar paths
      * Internal path to save avatars. Make sure this folder is writable. The slash at the end is VERY important!
