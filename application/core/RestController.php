d5cef376f8c53e4df5cc8ef4e2bead9da34315fe
Dylan Renwick <skidsirongenesisdev@gmail.com>
2018-11-18 22:09:57 -0500

    Add RestController for REST API endpoints

---
+++ application/core/RestController.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This is the base controller for all REST endpoint controllers.
+ * Whenever a controller is created, we also
+ * 1. initialize a session
+ * 2. check if the user is not logged in anymore (session timeout) but has a cookie
+ * 3. if the user is not logged in and there is no cookie, we check the request for an oauth token
+ */
+class RestController extends Controller
+{
+    /**
+     * Construct this object by extending the basic Controller class
+     */
+    public function __construct()
+    {
+        parent::__construct(true);
+        header('Content-Type: application/json');
+    }
+
+    /**
+     * Returns an array of implemented HTTP request methods.
+     * Child classes should implement methods by defining a function with the name of the method
+     * 
+     * @return array
+     */
+    public function getSupportedMethods()
+    {
+        $actions = array('HEAD', 'GET', 'POST', 'PUT', 'DELETE', 'TRACE', 'OPTIONS', 'CONNECT', 'PATCH');
+        $availableActions = array();
+        foreach($actions as $action) {
+            if (method_exists($this, $action)) {
+                $availableActions[] = $action;
+            }
+        }
+        return $availableActions;
+    }
+}
